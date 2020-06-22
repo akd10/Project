7f45a3593e6a9ee6d7d397a43c37c1860c6cd88f
Ankur Dwivedi <ankurkumar.20@tapmi.edu.in>
2020-06-23 00:31:12 +0530

    adds a python script

--- python_code.py
+++ python_code.py
@@ -1,5 +1,5 @@
-print("Hello! Github")
+print("This notebook will be mainly used for capstone projectt")
 import pandas as pd
 import numpy as np
-print("Hello Capstone Project Course"
+print("Hello Capstone Project Course")
 
